d81157483cceb0a5c61f62e182d3dd82597589e3
pedr0206 <ped@umbraco.dk>
2018-09-21 13:19:24 +0200

    Added Comic Book Detail view

--- src/CommicBookGallery/Controllers/ComicBooksController.cs
+++ src/CommicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace CommicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the controller");
+            return View();
         }
 
     }
